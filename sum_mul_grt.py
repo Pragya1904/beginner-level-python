e83259b56be48047a3b8081a4e419fe5eaec4679
Annaisha <77471889+Pragya1904@users.noreply.github.com>
2021-01-24 20:27:54 +0530

    initial commit

---
+++ sum_mul_grt.py
@@ -0,0 +1,15 @@
+num1=int(input("enter number 1: "))
+num2=int(input("enter number 2: "))
+print(num1)
+print(num2)
+sum=num1+num2
+print("sum of the two numbers:")
+print(sum)
+mul=num1*num2
+print("multiplication of the two numbers:")
+print(mul)
+
+if mul>100:
+	print("the product of the two numbers is greater than 100")
+else:
+	print("the product of the two numbers is smaller than 100")
