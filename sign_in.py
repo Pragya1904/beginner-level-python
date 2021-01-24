f160bd740ed2948864b8fab1458bfd8116d0ab3d
Annaisha <77471889+Pragya1904@users.noreply.github.com>
2021-01-24 20:27:10 +0530

    initial commit

---
+++ sign_in.py
@@ -0,0 +1,13 @@
+name=input("enter your name: ")
+age=input("enter your age: ")
+sex=input("enter your sex [M/F]: ")
+
+print("Name: " + name)
+print("Age: "+ age)
+
+if sex=="M":
+ print("Sex: " + sex)
+elif sex=="F":
+ print("Sex: " + sex)
+else:
+ print("enter sex as M/F only!")   
